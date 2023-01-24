f14c2a35484c6ba15021108b4f02501df313e966
mubeenasif01 <35574940+mubeenasif01@users.noreply.github.com>
2023-01-24 15:44:04 +0500

    -- Initial commit

--- Type Writer Animation/ViewController.swift
+++ Type Writer Animation/ViewController.swift
@@ -8,12 +8,96 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    var placeHolderText = ["Medicines",
+                           "Doctor",
+                           "Specialities",
+                           "Hospitals"]
+    
+    var initialText = "Search For "
+    
+    var myText : [[String.Element]]? = [[String.Element]]()
+    var placeHolderIndex = 0
+    var myCounter = 0
+    var timer:Timer?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textField.delegate = self
+        addPlaceholder(placeholder: initialText)
+        for obj in placeHolderText{
+            myText?.append(Array(obj))
+        }
+        fireTimer()
     }
-
-
+    
+    func fireTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(typeLetter), userInfo: nil, repeats: true)
+    }
+    
+    @objc func typeLetter(){
+        if let element = myText{
+            if myCounter < element[placeHolderIndex].count {
+                
+                self.addPlaceholder(placeholder: (textField.placeholder ?? "") + String(element[placeHolderIndex][myCounter]))
+                timer?.invalidate()
+                timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(typeLetter), userInfo: nil, repeats: false)
+            } else {
+                if placeHolderIndex == ((myText?.count ?? 0) - 1){
+                    placeHolderIndex = 0
+                }else{
+                    placeHolderIndex += 1
+                }
+                timer?.invalidate()
+                timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(reset), userInfo: nil, repeats: true)
+                return
+            }
+            myCounter += 1
+        }
+    }
+    
+    @objc func reset(){
+        timer?.invalidate()
+        self.myCounter = 0
+        self.fireTimer()
+        self.textField.placeholder = initialText
+    }
+    
+    func addPlaceholder(placeholder: String){
+        textField.attributedPlaceholder = NSAttributedString(
+            string: placeholder,
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.white]
+        )
+    }
 }
 
+extension ViewController: UITextFieldDelegate{
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        timer?.invalidate()
+        if textField.text == ""{
+            addPlaceholder(placeholder: "")
+            placeHolderIndex = 0
+        }
+        else{
+            
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField.text == ""{
+            myCounter = 0
+            placeHolderIndex = 0
+            addPlaceholder(placeholder: initialText)
+            fireTimer()
+        }
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
